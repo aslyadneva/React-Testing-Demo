d53e4d6243706cd8baa2d85562858f209d7efbba
Anastasia <slyadneva.anastasia@gmail.com>
2020-06-24 15:39:20 -0500

    Install and configure Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Enzyme from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
+// configuring Enzyme
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// if any errors are thrown in the anonymous function passed to the 'test' function, the test will fail
 test("renders without error", () => {});
